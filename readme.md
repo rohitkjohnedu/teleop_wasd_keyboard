c7aba0cdf904adac544d6d9d72142167c4d7b1b6
Rohit John <rohitkjohnedu@gmail.com>
2021-10-31 12:54:02 +0100

    Modified so that the standard meometry_msg is used
    I think its better to go with standard messages rather than my own custom message

--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
-# Aim
-To make my own teleop node. It works like racing game. WASD controls the speed. Space for braking
+# teleop_WASD_keyboard
+This is a node which listens to keyboard input and publishes a topic which can be used to operate a service robot moving on a plane. This was inspired by teleop_twist_keyboard [1]
+## Aim
+To make my own teleop node. It works like racing game. WASD controls the speed. Space for braking.
+
+## Reference
+[1] https://github.com/ros-teleop/teleop_twist_keyboard
+[2] https://stackoverflow.com/questions/11918999/key-listeners-in-python
--- src/teleop_wasd_keyboard/scripts/my_keyboard_listener.py
+++ src/teleop_wasd_keyboard/scripts/my_keyboard_listener.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-from pynput import keyboard
 import rospy
-from teleop_wasd_keyboard.msg import cmd_2D_robot_vel
-import time
 import math
 
+from geometry_msgs.msg import Twist
+from pynput import keyboard
+
+
+# -------------------------------------------------------------------------— Setting up
+# ------------------------------------------------------------------ Keymap
 valid_inputs = {
     # "letter":{"change":[vx, vy, wz], "state":None}
 
@@ -31,30 +34,76 @@ key_2_vel_idx = {
     'e':2,
 }
 
+# ------------------------------------------------------------------ Robot parameters
+Vel = [0, 0, 0]  
+dt  = 0.1 
+max_acc  = 1
+max_vel  = 10
 
+# ------------------------------------------------------------------------- Functions
+# ------------------------------------------------------------------ update_Velocity
 def update_Velocity():
+    """
+    Updates the velocity variable based on the state of control keys
+    """
     for key in valid_inputs.keys():
         if valid_inputs[key]["state"] == "down":
             if key == "space":
-                for idx in range(3):
-                    abs_change_vel = abs(Vel[idx]) - max_acc*dt
-                    abs_change_vel = max(abs_change_vel, 0)
-                    vel_sign       = math.copysign(1, Vel[idx])
-                    Vel[idx]       = vel_sign*abs_change_vel
+                calc_BrakeTrajectory()
 
             else:
-                vel_idx  = key_2_vel_idx[key]
-                sign     = valid_inputs[key]["change"][vel_idx]
-                cur_vel  = Vel[vel_idx]
-                next_vel = cur_vel + max_acc*dt*sign
+                calc_AccelerateTrajectory(key)
 
-                if abs(next_vel) > max_vel:
-                    next_vel = max_vel*sign
+# ------------------------------------------------------------------ assign_AccelerateCommand
+def calc_AccelerateTrajectory(key):
+    """
+    Calculates the velocity when accelerate command is registered and 
+    assigns it to the appropriate component of the velocity list
 
-                Vel[vel_idx] = next_vel
+    Parameters
+    ----------
+    key : int
+        The index of the velocity list
+    """
+
+    vel_idx  = key_2_vel_idx[key]
+    sign     = valid_inputs[key]["change"][vel_idx]
+    cur_vel  = Vel[vel_idx]
+    next_vel = cur_vel + max_acc*dt*sign
+
+    if abs(next_vel) > max_vel:
+        next_vel = max_vel*sign
+
+    Vel[vel_idx] = next_vel
+
+# ------------------------------------------------------------------ assign_BrakeCommand
+def calc_BrakeTrajectory():
+    """
+    Calculates the velocity when the brake command is registered and
+    assigns it to the velcoity list
+    """
+    for idx in range(3):
+        abs_change_vel = abs(Vel[idx]) - max_acc*dt
+        abs_change_vel = max(abs_change_vel, 0)
+        vel_sign       = math.copysign(1, Vel[idx])
+        Vel[idx]       = vel_sign*abs_change_vel
 
 
+# ------------------------------------------------------------------ callback_respond_ToKeyPress
 def callback_respond_ToKeyPress(key):
+    """
+    Callback function which is triggered when a key is pressed
+
+    Parameters
+    ----------
+    key : str
+        The key that is pressed
+
+    Returns
+    -------
+    bool
+        Should we stop listenning
+    """
     if key == keyboard.Key.esc:
         return False  # stop listener
     try:
@@ -65,8 +114,21 @@ def callback_respond_ToKeyPress(key):
     if key_name in valid_inputs.keys():
         valid_inputs[key_name]["state"] = "down"
 
+# ------------------------------------------------------------------ callback_respond_ToKeyRelease
+def callback_respond_ToKeyRelease(key):
+    """    
+    Callback function which is triggered when a key is released
 
-def callback_respond_ToKeyrelease(key):
+    Parameters
+    ----------
+    key : str
+        The key that is released
+
+    Returns
+    -------
+    bool
+        Should we stop listenning
+    """
     if key == keyboard.Key.esc:
         return False  # stop listener
     try:
@@ -77,34 +139,41 @@ def callback_respond_ToKeyrelease(key):
     if key_name in valid_inputs.keys():
         valid_inputs[key_name]["state"] = "up"
 
+# -------------------------------------------------------------------------— mane 
 if __name__ == '__main__':
-    Vel = [0, 0, 0]   
-    vx, vy, wz = Vel 
-    dt  = 2
-    max_acc  = 1
-    max_vel  = 10
-    listener = keyboard.Listener(on_press=callback_respond_ToKeyPress, on_release = callback_respond_ToKeyrelease)
+    # Initialise the listener
+    listener = keyboard.Listener(on_press=callback_respond_ToKeyPress, on_release = callback_respond_ToKeyRelease)
     listener.start()  # start to listen on a separate thread
     # listener.join()   # add if main thread is polling self.keys
     
-    
+    # Initialising the rosnode
     rospy.init_node("my_teleop")
-    pub  = rospy.Publisher("/my_cmd_topic", cmd_2D_robot_vel, queue_size = 1)
+    pub  = rospy.Publisher("/my_cmd_topic", Twist, queue_size = 1)
     rate = rospy.Rate(1/dt) 
-    msg  = cmd_2D_robot_vel()
 
-    msg.vx = vx
-    msg.vy = vy
-    msg.wz = wz
-    while True:
+    # initialising the message
+    msg        = Twist()
+    vx, vy, wz = Vel 
+    msg.linear.x  = vx
+    msg.linear.y  = vy
+    msg.linear.z  = 0.0
+
+    msg.angular.x = 0.0
+    msg.angular.y = 0.0
+    msg.angular.z = wz
+
+    # Running the node
+    while not rospy.is_shutdown():
+        # Calculate the velocity and assign it to the message
         update_Velocity()
         vx, vy, wz = Vel
-        msg.vx = vx
-        msg.vy = vy
-        msg.wz = wz
+        msg.linear.x  = vx
+        msg.linear.y  = vy
+        msg.angular.z = wz
+
         pub.publish(msg)
         rate.sleep()
 
         # Debug
-        # print(f'the velocity are vx: {vx}, vy: {vy}, wz: {wz}')
+        # rospy.loginfo(f'the velocity are vx: {vx}, vy: {vy}, wz: {wz}')
             
